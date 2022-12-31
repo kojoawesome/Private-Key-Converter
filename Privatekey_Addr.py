ed9c71e0f7b3370483c9a0d697d490d48fabfbb3
Kojo Gyan Kumi <kojokumi16.kk@gmail.com>
2022-12-31 11:57:34 +0000

    udate 3

--- Privatekey_Addr.py
+++ Privatekey_Addr.py
@@ -1,18 +1,23 @@
 from bit import Key
 
+count = 0
 #grabing file content
-file = "private keys.txt"
+file = 'private-keys.txt'
 with open (file, 'r', encoding='utf-8', errors='ignore') as reader:
-    for keys in reader:
-        private_key = keys
+    for keys in reader.readlines():
+        private_key = keys.strip('\n').strip()
         prv = Key.from_hex(private_key)
 
         addr = prv.address
-        print(addr)
+        print(f'{private_key}:{addr}\n')
         # balance = prv.get_balance('usd')
         # writing files locally
         with open ('Results by Codejoe.txt', 'a') as writer:
-            writer.write(f'{private_key}:{addr}')
+            writer.write(f'{private_key}:{addr}\n')
+        # count =+ 1
+        # if count == 100000:
             writer.close()
+
+
 
     
