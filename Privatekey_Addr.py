46591655d0a674822b21007d1f67dbfeeb5c8096
Kojo Gyan Kumi <kojokumi16.kk@gmail.com>
2023-01-02 00:32:15 +0000

    complete update

--- Privatekey_Addr.py
+++ Privatekey_Addr.py
@@ -1,5 +1,5 @@
 from bit import Key
-import sys
+from datetime import date
 
 print ('''
 
@@ -14,29 +14,36 @@ print ('''
 ========================================================    
      \n''')
 
-
+count = 0
 #grabing file content
 file = input('Enter file with .txt: \n')
 amount = int(input('No. of keys to convert: '))
+line_Starter = int(input('Enter line to beging from: '))
+print('Converting...')
+
 # file = 'Results by Codejoe.txt'
 with open (file, 'r', encoding='utf-8', errors='ignore') as reader:
-    # lines = reader.readlines()
-    for keys in reader.readlines():
-        private_key = keys.strip('\n').strip()
-        prv = Key.from_hex(private_key)
+    for i, line in enumerate(reader):
+        if i == line_Starter:
+            for keys in reader.readlines():
+                private_key = keys.strip('\n').strip()
+                prv = Key.from_hex(private_key)
+                addr = prv.address
+                # print(f'{private_key}:{addr}\n')
+                # balance = prv.get_balance('usd')
 
-        addr = prv.address
-        # print(f'{private_key}:{addr}\n')
-        # balance = prv.get_balance('usd')
-        count = 0
-        count += 1
-        # writing files locally
-        with open ('Results by Codejoe.txt', 'a') as writer:
-            writer.write(f'{private_key}:{addr}\n')
-        if count == amount:
-                writer.close()
-                sys.exit()
+                #Setting Range
 
-print(count)
 
+                count += 1
+                # writing files locally
+                with open ('Results by Codejoe.txt', 'a') as writer:
+                    writer.write(f'{private_key}:{addr}\n')
+                if count == amount:
+                    writer.close()
+                    break
+
+with open ('Logs.txt', 'a') as logger:
+    logger.write(f'line checkpoint: {line_Starter} Date: {date.today()}\n')
+    logger.close()
     
